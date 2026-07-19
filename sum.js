5a6f0b448dfc4eee8c9971019f54f1239bed559a
anemens3-oss <anemens3@gmail.com>
2026-07-19 12:37:50 +0000

    Commit6

---
+++ sum.js
@@ -0,0 +1,17 @@
+const modal = document.getElementById("modal");
+const openBtn = document.getElementById("openModal");
+const closeBtn = document.querySelector(".close");
+
+openBtn.addEventListener("click", () => {
+    modal.style.display = "flex";
+});
+
+closeBtn.addEventListener("click", () => {
+    modal.style.display = "none";
+});
+
+window.addEventListener("click", (event) => {
+    if (event.target === modal) {
+        modal.style.display = "none";
+    }
+});
